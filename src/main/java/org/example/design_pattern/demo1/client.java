d2d5a041434599da257ce6705c43b87f92f2ce1e
X431213 <lY431213>
2023-01-29 11:55:40 +0800

    提交到分支dev

--- src/main/java/org/example/design_pattern/demo1/client.java
+++ src/main/java/org/example/design_pattern/demo1/client.java
@@ -3,7 +3,7 @@ package org.example.design_pattern.demo1;
 public class client {
     public static void main(String[] args) {
         Boss boss=new Boss();
-        Employ e=new Employ("yy");
+        Employ e=new Employ("dd");
         e.addObserver(boss);
         e.setEvent();
     }
