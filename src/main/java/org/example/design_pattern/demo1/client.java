fda0f4b899a15c55f65605d378efb6fadb6a84ad
X431213 <lY431213>
2023-01-29 11:17:54 +0800

    测试fetch

--- src/main/java/org/example/design_pattern/demo1/client.java
+++ src/main/java/org/example/design_pattern/demo1/client.java
@@ -3,7 +3,7 @@ package org.example.design_pattern.demo1;
 public class client {
     public static void main(String[] args) {
         Boss boss=new Boss();
-        Employ e=new Employ("yby");
+        Employ e=new Employ("yy");
         e.addObserver(boss);
         e.setEvent();
     }
