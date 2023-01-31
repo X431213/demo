6a7b5500f14cdcea70c75916f23c5571fdb7f935
xiaoyufeng <xiaoyufeng>
2023-01-31 11:02:52 +0800

    分支2提交，测试分支1pull

--- src/main/java/org/example/design_pattern/obser/demo1/client.java
+++ src/main/java/org/example/design_pattern/obser/demo1/client.java
@@ -3,7 +3,7 @@ package org.example.design_pattern.obser.demo1;
 public class client {
     public static void main(String[] args) {
         Boss boss=new Boss();
-        Employ e=new Employ("ssb");
+        Employ e=new Employ("123");
         e.addObserver(boss);
         e.setEvent();
     }
